8760e32214f1313b3e50321dea2de24fa9c06664
himntadk <himantabiswadeka234@gmail.com>
2023-07-02 22:36:18 +0530

    commited by himanta

---
+++ venv/app.py
@@ -0,0 +1,36 @@
+from flask import Flask,render_template,request
+from flask_sqlalchemy import SQLAlchemy
+from flask_mysqldb import MySQL
+
+app = Flask(__name__)
+
+app.config['MYSQL_HOST']= 'localhost'
+app.config['MYSQL_USER']= 'root'
+app.config['MYSQL_PASSWORD']= ''
+app.config['MYSQL_DB']= 'pythondb'
+
+mysql= MySQL(app)
+
+
+@app.route("/")
+def home():
+    return "Hello User!!!"
+
+@app.route("/register",methods =['POST','GET'])
+def register():
+    if request.method== 'GET':
+       return render_template("register.html")
+    
+    if request.method== 'POST':
+       name = request.form['name']
+       email = request.form['email']
+       phone = request.form['phone']
+       college = request.form['college']
+       cursor = mysql.connection.cursor()
+       cursor.execute('''INSERT INTO user(name,email,phone,college) VALUES(%s,%s,%s,%s)''',(name,email,phone,college))
+       mysql.connection.commit()
+       cursor.close()
+       return "Data inserted successfully"
+
+if __name__ == "__main__":
+  app.run(host= 'localhost')
